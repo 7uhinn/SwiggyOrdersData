278dbc5d7020373354d9680446b9b591dba1ba63
Tuhin Sarkar <tuhin_sarkar@srmap.edu.in>
2023-06-29 19:21:22 +0530

    Added data columns from response into CSV

--- GenerateData.py
+++ GenerateData.py
@@ -3,6 +3,7 @@ import json
 import sys
 from rich import print
 import pandas as pd
+import ast
 
 HEADERS = {
     'Host': 'www.swiggy.com',
@@ -18,13 +19,26 @@ GET_ORDERS_URL = 'https://www.swiggy.com/dapi/order/all?order_id='
 
 def getOrders(cookies):
     print("Retrieving...")
-    spent = 0
+    
     s = requests.Session()
     last_order_id = ''
-    num_of_orders = 0
+    idx = 1
 
     f = open("SwiggyOrdersData.csv", "w")
-    f.write('order_total'+","+'order_id'+","+"delivery_address")
+    f.write('S. No.'+","
+            +'Order ID'+","
+            +'Order Time'+","
+            +'Restaurant'+","
+            +'Items'+","
+            +'Address'+","
+            +'Order Status'+","
+            +'Order Amount'+","
+            +'Swiggy Payment Method'+","
+            +'Swiggy Payment Display Name'+","
+            +'Swiggy Payment VPA'+","
+            +'Payment Gateway Payment Type'+","
+            +'Payment Gateway Payment Card'+","
+            +'Payment Gateway Payment Issuer')
 
     while 1:
         # 10 orders retrieved in each api call
@@ -46,9 +60,53 @@ def getOrders(cookies):
             break
 
         for order in resp['data']['orders']:
-            f.write("\n"+str(order['order_total'])+"," +
-                    str(order['order_id'])+"," +
-                    str(order['delivery_address']['address']).replace(",", ""))
+            sno = str(idx)
+            order_id = str(order['order_id'])
+            order_time = str(order['order_time'])
+            restaurant = str(order['restaurant_name']).replace(",", "")
+            
+            item_names = ""
+            for items in order['order_items']:
+                item_names += items['name']+" "
+            item_names = item_names.replace(",", "")
+
+            flat_no = str(order['delivery_address']['flat_no']).replace(",", "")
+            location = str(order['delivery_address']['address']).replace(",", "")
+            area = str(order['delivery_address']['area']).replace(",", "")
+            address = flat_no+" "+location+" "+area
+
+            order_status = str(order['order_status']).replace(",", "")
+            order_amount = str(order['order_total'])
+            swiggy_payment_method = str(order['paymentTransactions'][0]['paymentMethod']).replace(",", "")
+            swiggy_payment_display_name = str(order['paymentTransactions'][0]['paymentMethodDisplayName']).replace(",", "")
+            swiggy_payment_vpa = str(order['paymentTransactions'][0]['paymentMeta']['payerVpa']).replace(",", "")
+
+            try:
+                externalPGResponse = json.loads(order['paymentTransactions'][0]['paymentMeta']['extPGResponse'])
+                payment_gateway_payment_type = str(externalPGResponse['paymentMethod']).replace(",", "")
+                payment_gateway_payment_card = str(externalPGResponse['card']['cardType']).replace(",", "")
+                payment_gateway_payment_issuer = str(externalPGResponse['card']['cardIssuer']).replace(",", "")
+            except Exception as e:
+                payment_gateway_payment_type = ""
+                payment_gateway_payment_card = ""
+                payment_gateway_payment_issuer = ""
+
+            f.write("\n"+sno+","
+                    +order_id+","
+                    +order_time+","
+                    +restaurant+","
+                    +item_names+","
+                    +address+","
+                    +order_status+","
+                    +order_amount+","
+                    +swiggy_payment_method+","
+                    +swiggy_payment_display_name+","
+                    +swiggy_payment_vpa+","
+                    +payment_gateway_payment_type+","
+                    +payment_gateway_payment_card+","
+                    +payment_gateway_payment_issuer)
+            
+            idx+=1
 
         last_order_id = resp['data']['orders'][-1]['order_id']
 
